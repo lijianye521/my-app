8b2a90913a79ae1519a9b03b31a1b468540ba5ab
lijianye521 <2548154814@qq.com>
2025-08-29 19:55:54 +0800

    feat: enhance PostCSS configuration by adding additional plugins for improved CSS processing and compatibility

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,25 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+    plugins: {
+        'postcss-flexbugs-fixes': {},
+        '@tailwindcss/postcss': {},
+        'postcss-preset-env': {
+            features: {
+                'nesting-rules': true,
+                'custom-properties': true
+            },
+            autoprefixer: {
+                grid: true,
+                flexbox: 'no-2009'
+            }
+        },
+        '@csstools/postcss-oklab-function': {
+            preserve: true
+        },
+        'postcss-color-function': {
+            preserve: true
+        }
+    }
+}
 
-export default config;
+export default config
