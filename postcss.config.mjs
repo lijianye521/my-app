22949337d8bd6076b98f68feb165da35fb9e3b32
lijianye521 <2548154814@qq.com>
2025-09-08 22:26:05 -1000

    chore: downgrade React and React-DOM to version 18.3.1; update related dependencies in package-lock.json for compatibility

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-    plugins: {
-        'postcss-flexbugs-fixes': {},
+    plugins: { // 'postcss-flexbugs-fixes': {},
         '@tailwindcss/postcss': {},
-        'postcss-preset-env': {
-            features: {
-                'nesting-rules': true,
-                'custom-properties': true
-            },
-            autoprefixer: {
-                grid: true,
-                flexbox: 'no-2009'
-            }
-        },
+        // 'postcss-preset-env': {
+        //     features: {
+        //         'nesting-rules': true,
+        //         'custom-properties': true
+        //     },
+        //     autoprefixer: {
+        //         grid: true,
+        //         flexbox: 'no-2009'
+        //     }
+        // },
         '@csstools/postcss-oklab-function': {
             preserve: true
         },
